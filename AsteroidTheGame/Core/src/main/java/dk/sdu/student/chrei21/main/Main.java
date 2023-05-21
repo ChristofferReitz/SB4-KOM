b0085b720fa7aa3b6fd2f0bb8eadaab61d2df77b
ChristofferReitz <chrei21@student.sdu.dk>
2023-05-22 00:01:19 +0200

    Revert "JavaLab"
    This reverts commit 20ef73ae0d395dd0c9c5e70465d0975f7927b580.

--- AsteroidTheGame/Core/src/main/java/dk/sdu/student/chrei21/main/Main.java
+++ AsteroidTheGame/Core/src/main/java/dk/sdu/student/chrei21/main/Main.java
@@ -4,20 +4,19 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 public class Main {
-
-
+	
 	public static void main(String[] args) {
-
+		
 		LwjglApplicationConfiguration cfg =
-				new LwjglApplicationConfiguration();
+			new LwjglApplicationConfiguration();
 		cfg.title = "Asteroids";
-		cfg.width = 1500;
-		cfg.height = 1000;
+		cfg.width = 500;
+		cfg.height = 400;
 		cfg.useGL30 = false;
-		cfg.resizable = true;
-
+		cfg.resizable = false;
+		
 		new LwjglApplication(new Game(), cfg);
-
+		
 	}
 	
 }
